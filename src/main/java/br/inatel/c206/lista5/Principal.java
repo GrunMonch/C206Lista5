84b46c1487f90214efee6b3a6c63ab7cf54e67a9
Irineu <irineo.rocha@gmail.com>
2021-11-14 21:15:51 -0300

    Classes CSV e Principal

--- src/main/java/br/inatel/c206/lista5/Principal.java
+++ src/main/java/br/inatel/c206/lista5/Principal.java
@@ -1,7 +1,13 @@
 package br.inatel.c206.lista5;
 
+import br.inatel.c206.lista5.csv.CSV;
+
+import java.nio.file.*;
+
 public class Principal {
     public static void main(String[] args) {
+        Path arquivo = Paths.get("funcionarios.csv");
+        CSV.generateCSVFuncs(arquivo);
 
     }
 }
